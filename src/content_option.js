ce0565ebacccd712c0f779c0bd22cb0c9dd920e0
Yuuma79 <52548770+Yuuma79@users.noreply.github.com>
2023-03-28 19:50:36 -0400

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -11,7 +11,7 @@ const introdata = {
         second: "I create Branding Identity",
         third: "I do UX Design",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "A designer that's all about solving problems and making sure products are accessible to everyone.",
     your_img_url: "https://images.unsplash.com/photo-1626785774573-4b799315345d",
 };
 
